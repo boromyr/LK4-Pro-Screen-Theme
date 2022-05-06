17c68606738ab8b7431a654f531d13824fba5e1a
boromyr <52782032+boromyr@users.noreply.github.com>
2022-05-06 14:19:52 +0200

    new greed icons

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -1,4 +1,4 @@
-call rename.bat
+@REM call rename.bat
 
 md release
 md release\DWIN_SET
@@ -10,3 +10,4 @@ copy DWIN_SET\*.* release\DWIN_SET
 rem rotate images
 for %%f in (release\DWIN_SET\*.bmp) do magick.exe convert -rotate -90 %%f %%f
 
+xcopy D:\Users\PC\Documents\GitHub\DWIN_LK4Pro\release\DWIN_SET L:\DWIN_SET\ /y
